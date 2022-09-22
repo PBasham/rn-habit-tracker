98c2619af76645a4dab914b20d4ce86dbe550b54
patrickbasham <PatrickTBasham@gmail.com>
2022-09-21 21:45:48 -0400

    fixed goalsContainer to have transparent background without affecting rest of content

--- src/components/HomeScreen/GoalsContainer.jsx
+++ src/components/HomeScreen/GoalsContainer.jsx
@@ -1,15 +1,34 @@
 /*========================================
         Import Dependencies
 ========================================*/
+import { useState } from "react"
 import { StyleSheet, Text, View, ScrollView } from 'react-native'
 import colors from "../../misc/colors"
+import HeaderOne from "../Text/HeaderOne"
 
 const GoalsContainer = () => {
+
+    const [todaysGoals, SetTodaysGoals] = useState([])
+
     return (
         <View style={styles.goalsContainer}>
-            <ScrollView>
-                <Text>You don't currently have any goals for today!</Text>
-            </ScrollView>
+            <View style={styles.backgroundView}>
+            </View>
+            {todaysGoals.length > 0 ?
+                <ScrollView>
+                </ScrollView>
+                :
+                <HeaderOne
+                    style={{
+                        flex: 1,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        opacity: .5,
+                        padding: 10,
+                    }}
+                    content={"You don't currently have any goals for today!"}
+                />
+            }
         </View>
     )
 }
@@ -18,8 +37,16 @@ export default GoalsContainer
 
 const styles = StyleSheet.create({
     goalsContainer: {
+        position: "relative",
         height: "50%",
+    },
+    backgroundView: {
+        position: "absolute",
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
         backgroundColor: colors.greyish,
-        opacity: .1,
+        opacity: .2,
     },
 })
