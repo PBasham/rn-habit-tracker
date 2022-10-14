3369c8f224f61b9617d95d913405c2738d256a18
patrickbasham <PatrickTBasham@gmail.com>
2022-10-14 16:41:50 -0400

    set up base file for Create note modal

---
+++ src/components/JournalScreen/CreateNoteModal.tsx
@@ -0,0 +1,24 @@
+/*========================================
+        Import Dependencies
+========================================*/
+import React, { FC } from 'react'
+import { View, StyleSheet, Modal } from 'react-native'
+
+interface CreateNoteModalProps {
+    
+}
+
+export const CreateNoteModal: FC<CreateNoteModalProps> = ({}) => {
+
+   return (
+       <Modal style={styles.container}>
+           
+       </Modal>
+   )
+}
+
+const styles = StyleSheet.create({
+   container: {
+       
+   }
+})
